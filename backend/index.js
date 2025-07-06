c899cb52b7c2695a2589fd5d26ea1b251ae6eacf
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-06 12:32:12 +0530

    ✅ Fix slash

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: "https://portfolio-website-rho-liart-85.vercel.app",
+    origin: "https://portfolio-website-three-sooty-71.vercel.app",
     credentials: true,
   })
 );

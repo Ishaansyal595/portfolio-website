9416b5e6cfde57042d4f368cf95a8beebfcf5394
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-06 14:17:33 +0530

    🌐 Update edit project page

--- frontend/src/components/ProjectsSection.jsx
+++ frontend/src/components/ProjectsSection.jsx
@@ -26,6 +26,8 @@ const ProjectsSection = () => {
     fetchProjects();
   }, []);
 
+  console.log(projects);
+
   return (
     <section id="projects" className="py-24 px-4 relative">
       <div className="container mx-auto max-width-5xl">

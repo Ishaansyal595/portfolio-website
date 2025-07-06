598fbde4a83dede4e9e6d6029d8b95cabf2176b1
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-06 09:12:04 +0530

    ✅ Fix dotenv import

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connectDB } from "./connectDb.js";
 import projectRouter from "./project.route.js";
 import cors from "cors";
+import { dotenv } from 'dotenv';
 
 const app = express();
 

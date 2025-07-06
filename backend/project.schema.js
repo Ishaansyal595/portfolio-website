62424eb52e8c8a96fd6861120ea1ed1f654f6bb8
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-06 13:41:11 +0530

    ✅ Fix schema

--- backend/project.schema.js
+++ backend/project.schema.js
@@ -1,12 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const projectSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    features: {type: [String], required: true},
-    technology: {type: [String], required: true},
-    images: {type: [String], required: true},
-})
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  features: { type: [String], required: true },
+  technology: { type: [String], required: true },
+  images: { type: [String], required: true },
+  github: { type: String }, // ✅ New field
+  live: { type: String },
+});
 
-const Project = mongoose.model("Project", projectSchema)
-export default Project
+const Project = mongoose.model("Project", projectSchema);
+export default Project;

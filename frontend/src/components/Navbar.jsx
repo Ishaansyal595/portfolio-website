a7fe940eb8d144f23fd856ac7d0e194a5c9d75db
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-09-02 12:00:14 +0530

    Updated portfolio with new changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import utils from "./../lib/utils";
 import { Menu, X } from "lucide-react";
 
 const navItems = [
-  { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
